3921e4a975b1ae796a6701d677fa47562ec3ff96
nvpnathan <nathan79@gmail.com>
2017-02-11 14:44:33 -0700

    Update 3-create-flavors.sh

--- 3-create-flavors.sh
+++ 3-create-flavors.sh
@@ -13,11 +13,12 @@ project4=orgBDept2
 
 #create projects
 photon tenant set $tenant1
-photon project set $project1
+#photon project set $project1
 echo y | photon flavor create --name "tiny-vm" --kind "vm" --cost "vm.cpu 1 COUNT, vm.memory 512 MB, vm.cost 1 COUNT"
 echo y | photon flavor create --name "small-vm" --kind "vm" --cost "vm.cpu 1 COUNT, vm.memory 1 GB, vm.cost 2 COUNT"
 echo y | photon flavor create --name "medium-vm" --kind "vm" --cost "vm.cpu 2 COUNT, vm.memory 2 GB, vm.cost 3 COUNT"
 
+: '
 photon tenant set $tenant1
 photon project set $project2
 echo y | photon flavor create --name "tiny-vm" --kind "vm" --cost "vm.cpu 1 COUNT, vm.memory 512 MB, vm.cost 1 COUNT"
@@ -35,4 +36,4 @@ photon project set $project4
 echo y | photon flavor create --name "tiny-vm" --kind "vm" --cost "vm.cpu 1 COUNT, vm.memory 512 MB, vm.cost 1 COUNT"
 echo y | photon flavor create --name "small-vm" --kind "vm" --cost "vm.cpu 1 COUNT, vm.memory 1 GB, vm.cost 2 COUNT"
 echo y | photon flavor create --name "medium-vm" --kind "vm" --cost "vm.cpu 2 COUNT, vm.memory 2 GB, vm.cost 3 COUNT"
-
+'
