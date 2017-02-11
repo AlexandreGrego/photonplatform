38988e75e37dc94bd3c1ee0cbdda71222b4f05cb
nvpnathan <nathan79@gmail.com>
2017-02-11 16:55:15 -0700

    Create 6-create-cluster-types.sh

---
+++ 6-create-cluster-types.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -x #echo on
+
+#fetch the required variables
+deploymentuuid=$(photon deployment list | awk 'FNR == 2 {print}')
+k8simageuuid=$(photon image list | grep kubernetes | awk '{print $1}')
+harborimageuuid=$(photon image list | grep kubernetes | awk '{print $1}')
+
+#enable kubernetes as a cluster-type
+photon -n deployment enable-cluster-type $deploymentuuid -k KUBERNETES -i $k8simageuuid
+
+#enable harbor registry as a cluster-type
+photon -n deployment enable-cluster-type $deploymentuuid -k HARBOR -i $harborimageuuid
