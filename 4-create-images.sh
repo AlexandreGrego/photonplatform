997e69d607c2e94fbc911fb37891c92edc571d71
nvpnathan <nathan79@gmail.com>
2017-02-11 20:59:05 -0700

    Update 4-create-images.sh

--- 4-create-images.sh
+++ 4-create-images.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 set -x #echo on
 
-#tenants
+#tenant variables
 tenant1=orgADevs
 tenant2=orgBDevs
 
-#projects
+#project variables
 project1=orgADept1
 project2=orgADept2
 project3=orgBDept1
 project4=orgBDept2
 
-#images
+#image variables
 k8s="images/kubernetes-1.4.3-pc-1.1.0-5de1cb7.ova"
-harbor="harbor-0.4.1-pc-1.1.0-5de1cb7.ova"
+harbor="images/harbor-0.4.1-pc-1.1.0-5de1cb7.ova"
 
 #import images
 photon tenant set $tenant1
 photon project set $project1
-photon image create "$k8s" -n kubernetes -i EAGER
-photon image create "$harbor" -n harbor -i ON_DEMAND
 
+#import kubernetes image
+photon image create "$k8s" -n kubernetes -i EAGER
+#import harbor image
+photon image create "$harbor" -n harbor -i ON_DEMAND
