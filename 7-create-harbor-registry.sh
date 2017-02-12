f79a712d7d9ab8453cab9b055e36c977b81873e8
nvpnathan <nathan79@gmail.com>
2017-02-11 21:17:31 -0700

    Update 7-create-harbor-registry.sh

--- 7-create-harbor-registry.sh
+++ 7-create-harbor-registry.sh
@@ -8,9 +8,6 @@ dns=192.168.110.10
 gw=192.168.110.1
 mask=255.255.255.0
 masterip=192.168.110.25
-etcdip=192.168.110.26
-containernet=10.2.0.0/16
-workers=1
 flavor=medium-vm
 
 #create harbor registry
