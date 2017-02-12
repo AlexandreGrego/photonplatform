7bd76bffcd3a590f21552be885aa839be7407225
nvpnathan <nathan79@gmail.com>
2017-02-11 21:07:39 -0700

    Update 4-create-networks.sh

--- 4-create-networks.sh
+++ 4-create-networks.sh
@@ -7,7 +7,7 @@ portgroup1="Tenant Network"
 desc1="Tenant Network"
 
 #create tenant network and copy uuid of this network to set the default-network
-uuid=$(photon network create --name $name1 --portgroups "portgroup1" --description "$desc1" | awk '{print $7}')
+uuid=$(photon network create --name $name1 --portgroups "$portgroup1" --description "$desc1" | awk '{print $7}')
 
 #set tenant network as the default network
 echo y | photon network set-default $uuid
