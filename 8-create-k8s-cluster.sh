3b8a324829569ddab229a30e5587d1ce23ff9ac3
nvpnathan <nathan79@gmail.com>
2017-02-12 21:15:33 -0700

    Update 8-create-k8s-cluster.sh

--- 8-create-k8s-cluster.sh
+++ 8-create-k8s-cluster.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -x #echo on
 
+#tenant variables
+tenant1=orgADevs
+
+#project variables
+project1=orgADept1
+
+photon tenant set $tenant1
+photon project set $orgADept1
 #variables
 name=k8cluster01
 dns=192.168.110.10
@@ -11,6 +19,10 @@ etcdip=192.168.110.26
 containernet=10.2.0.0/16
 workers=1
 flavor=medium-vm
+
+
+photon tenant set $tenant1
+photon project set $orgADept1
 
 #create k8s cluster
 photon cluster create -n $name -k KUBERNETES --dns $dns --gateway $gw --netmask $mask --master-ip $masterip --container-network "$containernet" --etcd1 $etcdip -c $workers -v $flavor
