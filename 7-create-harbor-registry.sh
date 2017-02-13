fdb1c90a8545eee5f4d033cefed1f3c0b7251f36
nvpnathan <nathan79@gmail.com>
2017-02-12 21:14:44 -0700

    Update 7-create-harbor-registry.sh

--- 7-create-harbor-registry.sh
+++ 7-create-harbor-registry.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -x #echo on
 
+#tenant variables
+tenant1=orgADevs
+
+#project variables
+project1=orgADept1
+
 #variables
 pass="VMware1!"
 name=habor01
@@ -9,6 +15,9 @@ gw=192.168.110.1
 mask=255.255.255.0
 masterip=192.168.110.20
 flavor=medium-vm
+
+photon tenant set $tenant1
+photon project set $orgADept1
 
 #create harbor registry
 photon cluster create -n $name -k HARBOR --dns $dns --gateway $gw --netmask $mask --master-ip $masterip -v $flavor --admin-password $pass
