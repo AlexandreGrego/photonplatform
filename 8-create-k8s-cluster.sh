55cc279ab034d396431326cd9931fd29e835768d
nvpnathan <nathan79@gmail.com>
2017-02-14 14:16:48 -0700

    Update 8-create-k8s-cluster.sh

--- 8-create-k8s-cluster.sh
+++ 8-create-k8s-cluster.sh
@@ -19,10 +19,11 @@ etcdip=192.168.100.26
 containernet=10.2.0.0/16
 workers=1
 flavor=medium-vm
-
+sshkey="../.ssh/id_rsa.pub"
+registryca=ca_harbor.crt
 
 photon tenant set $tenant1
 photon project set $project1
 
 #create k8s cluster
-photon cluster create -n $name -k KUBERNETES --dns $dns --gateway $gw --netmask $mask --master-ip $masterip --container-network "$containernet" --etcd1 $etcdip -c $workers -v $flavor
+photon cluster create -n $name -k KUBERNETES --dns $dns --gateway $gw --netmask $mask --master-ip $masterip --container-network "$containernet" --etcd1 $etcdip -c $workers -v $flavor --ssh-key $sshkey --registry-ca-cert $registryca
