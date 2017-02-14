4b7d3ea61119d02b7e86da2ed7c1f14742d1694d
nvpnathan <nathan79@gmail.com>
2017-02-14 12:13:52 -0700

    Update 7-create-harbor-registry.sh

--- 7-create-harbor-registry.sh
+++ 7-create-harbor-registry.sh
@@ -9,18 +9,20 @@ project1=orgADept1
 
 #variables
 pass="VMware1!"
-name=habor01
-dns=192.168.110.10
-gw=192.168.100.1
+name=harbor01
+dns=192.168.64.60
+gw=192.168.72.1
 mask=255.255.255.0
-masterip=192.168.100.20
+masterip=192.168.72.20
 flavor=medium-vm
+sshkey="../.ssh/id_rsa.pub"
 
 photon tenant set $tenant1
 photon project set $project1
 
 #create harbor registry
-harboruuid=$(photon cluster create -n $name -k HARBOR --dns $dns --gateway $gw --netmask $mask --master-ip $masterip -v $flavor --admin-password $pass | awk '{print $6}')
+echo y | photon cluster create -n $name -k HARBOR --dns $dns --gateway $gw --netmask $mask --master-ip $masterip -v $flavor --admin-password $pass --ssh-key "$sshkey"
 
+harboruuid=$(photon cluster list | grep $name | awk '{print $1}')
 #show the harbor deployment and save the certificate
 photon cluster show $harboruuid
