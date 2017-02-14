63c990c8f3f235ac9ca4235c9e97f4c415157818
nvpnathan <nathan79@gmail.com>
2017-02-14 16:21:36 -0700

    Update 7-create-harbor-registry.sh

--- 7-create-harbor-registry.sh
+++ 7-create-harbor-registry.sh
@@ -23,9 +23,13 @@ photon project set $project1
 #create harbor registry
 echo y | photon cluster create -n $name -k HARBOR --dns $dns --gateway $gw --netmask $mask --master-ip $masterip -v $flavor --admin-password $pass --ssh-key "$sshkey"
 
+#save the certificate to file
+harboruuid=$(photon cluster list | grep $name | awk '{print $1}')
+photon cluster cert-to-file $harboruuid $name_cert.crt
+
 #save certificate with openssl s_client
 #openssl s_client -showcerts -connect $masterip:443 </dev/null 2>/dev/null | sed -n '/-----BEGIN CERTIFICATE-----/,/-----END CERTIFICATE-----/p' | grep -m1 -B 40 -- '-----END CERTIFICATE-----'  > $name_ca_harbor.crt
 
 #save certificate with photon cli
-harboruuid=$(photon cluster list | grep $name | awk '{print $1}')
-photon cluster show $harboruuid | sed -n 's/.*ca_cert://;/-----BEGIN CERTIFICATE-----/,/-----END CERTIFICATE-----/p' > $name_cert.crt
+#harboruuid=$(photon cluster list | grep $name | awk '{print $1}')
+#photon cluster show $harboruuid | sed -n 's/.*ca_cert://;/-----BEGIN CERTIFICATE-----/,/-----END CERTIFICATE-----/p' > $name_cert.crt
