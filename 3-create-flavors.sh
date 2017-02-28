954d28e9762be2840de5c7117924f340bbbf42f7
nvpnathan <nathan79@gmail.com>
2017-02-28 15:04:19 -0700

    Update 3-create-flavors.sh

--- 3-create-flavors.sh
+++ 3-create-flavors.sh
@@ -31,6 +31,11 @@ mediumname=medium-vm
 mediumcpu=2
 mediummem=2
 mediumcost=3
+#large-vm
+largename=large-vm
+largecpu=4
+largemem=4
+largecost=4
 #ephemeral-vm-disk
 ephemeraldiskname1=vm-disk
 diskcount1=1
@@ -44,6 +49,7 @@ clusterephemeraldiskname1=cluster-vm-disk
 echo y | photon flavor create --name "$tinyname" --kind "$vm" --cost "vm.cpu $tinycpu COUNT, vm.memory $tinymem MB, vm.cost $tinycost COUNT"
 echo y | photon flavor create --name "$smallname" --kind "$vm" --cost "vm.cpu $smallcpu COUNT, vm.memory $smallmem GB, vm.cost $smallcost COUNT"
 echo y | photon flavor create --name "$mediumname" --kind "$vm" --cost "vm.cpu $mediumcpu COUNT, vm.memory $mediummem GB, vm.cost $mediumcost COUNT"
+echo y | photon flavor create --name "$largename" --kind "$vm" --cost "vm.cpu $largecpu COUNT, vm.memory $largemem GB, vm.cost $largecost COUNT"
 echo y | photon flavor create --name "$ephemeraldiskname1" --kind "$ephemeraldisk" --cost "ephemeral-disk $diskcount1 COUNT, ephemeral-disk.flavor.$ephemeraldiskname1 1 COUNT, ephemeral-disk.cost $diskcost1 COUNT"
 echo y | photon flavor create --name "$clusterephemeraldiskname1" --kind "$ephemeraldisk" --cost "ephemeral-disk $diskcount1 COUNT, ephemeral-disk.flavor.$ephemeraldiskname1 1 COUNT, ephemeral-disk.cost $diskcost1 COUNT"
 echo y | photon flavor create --name "$persistentdiskname1" --kind "$persistentdisk" --cost "persistent-disk $diskcount1 COUNT, persistent-disk.flavor.$persistentdiskname1 1 COUNT, persistent-disk.cost $diskcost1 COUNT"
