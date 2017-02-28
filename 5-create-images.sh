58c7c635bb603e1acabad5aadd13bc164765fd8c
nvpnathan <nathan79@gmail.com>
2017-02-28 15:11:33 -0700

    Update 5-create-images.sh

--- 5-create-images.sh
+++ 5-create-images.sh
@@ -14,6 +14,7 @@ project4=orgBDept2
 #image variables
 k8s="images/kubernetes-1.5.1-pc-1.1.1-dfea3bb.ova"
 harbor="images/harbor-0.4.1-pc-1.1.1-dfea3bb.ova"
+docker="images/debian-8.2.vmdk"
 
 #import images
 
@@ -21,3 +22,5 @@ harbor="images/harbor-0.4.1-pc-1.1.1-dfea3bb.ova"
 photon image create "$k8s" -n kubernetes -i EAGER
 #import harbor image
 photon image create "$harbor" -n harbor -i ON_DEMAND
+#import docker-machine image
+photon image create "$docker" -n "docker-machine" -i EAGER
