ff3c8a3941f6b392a0c61b04541d43050438defa
nvpnathan <nathan79@gmail.com>
2017-03-06 19:50:31 -0700

    Update 8-create-k8s-cluster.sh

--- 8-create-k8s-cluster.sh
+++ 8-create-k8s-cluster.sh
@@ -7,10 +7,10 @@ set -x #echo on
 #variables
 name=k8cluster01
 dns=192.168.110.10
-gw=192.168.100.1
+gw=192.168.110.1
 mask=255.255.255.0
-masterip=192.168.100.25
-etcdip=192.168.100.26
+masterip=192.168.110.25
+etcdip=192.168.110.26
 containernet=10.2.0.0/16
 workers=1
 flavor=medium-vm
