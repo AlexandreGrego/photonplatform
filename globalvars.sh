787bb45cd3818dd6bc602da8158ab919a6a84f93
nvpnathan <nathan79@gmail.com>
2017-03-06 19:51:12 -0700

    Update globalvars.sh

--- globalvars.sh
+++ globalvars.sh
@@ -5,7 +5,7 @@ tenant1=orgADevs
 tenant2=orgBDevs
 
 #project variables
-project1=k8s-project
-project2=docker-project
+project1=orgADept1
+project2=orgBDept2
 project3=orgBDept1
 project4=orgBDept2
