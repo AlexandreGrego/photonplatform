d5f71f8411c88bfa551793a8acdfd9e7badd936a
nvpnathan <nathan79@gmail.com>
2017-03-21 07:47:41 -0600

    Update login.sh

--- login.sh
+++ login.sh
@@ -4,8 +4,8 @@ set -x #echo on
 #Variables
 user=Administrator
 pass="VMware1!"
-tenant=photoncloud
-api=https://192.168.110.90:443
+tenant=corp.local
+api=https://192.168.110.95:443
 
 #login
 echo y | photon target set -c $api && photon target login -u $user@$tenant -p $pass
