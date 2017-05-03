07c740d8332a346a515dcf5aa0dc52dd5f339b6b
root <root@photon-dev.corp.local>
2017-05-03 16:41:38 +0000

    updated k8s

--- 8-create-k8s-cluster.sh
+++ 8-create-k8s-cluster.sh
@@ -19,3 +19,23 @@ photon project set $project1
 
 #create k8s cluster
 echo y | photon service create -n $name -k KUBERNETES --number-of-masters $masternum --container-network "$containernet" --number-of-etcds $etcdnum -c $workers -v $flavor --ssh-key $sshkey --registry-ca-cert $registryca
+
+#Variables
+user=Administrator
+pass="VMware1!"
+tenant=photon.local
+rm -f pc_login_info
+
+clustername="k8cluster01"
+clusterid=$(photon cluster list |grep "$clustername" |awk '{print $1}')
+
+#Create login File
+printf "$user@$tenant\n$pass\n" >>pc_login_info
+
+#Find Master IP of Cluster
+ip=$(photon cluster show $clusterid |grep " master_ips" |sed -r 's/.* master_ips:([^ ]+).*/\1/'i)
+
+#Copy File to Master
+
+scp  -o StrictHostKeyChecking=no -i /root/.ssh/id_rsa pc_login_info root@$ip:/etc/kubernetes/pc_login_info
+
